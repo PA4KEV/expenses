933157c97e238509be9a5cc8ea647d8ef353932e
PA4KEV <kevin.vd.vleuten@gmail.com>
2023-01-15 13:09:35 +0100

    Move axios API call to inner body

--- frontend/src/UI/DataRetrieve.js
+++ frontend/src/UI/DataRetrieve.js
@@ -1,35 +1,31 @@
 import React from 'react';
 import axios from 'axios';
 
-async function connectionTest() {
-    try{
-        const response = await axios.get('/connection_test');
-        console.log('response  ', response)
-        return response.data;
-    } catch(error) {
-        return [];
-    }    
-}
-
-async function getExpenses() {
-    try{
-        const response = await axios.get('/get_expenses/year/2023');
-        console.log('response  ', response)
-        return response.data;
-    } catch(error) {
-        return [];
-    }    
-}
-
-
 const DataRetrieve = (props) => {
-    
+    const [expense, setPost] = React.useState(null);
 
-    connectionTest();
-    getExpenses();
+    let year = 2023;
+    const baseURL = "/get_expenses/year/" + year;
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+          setPost(response.data);
+        });
+      }, []);
+    
+    if (!expense) return( <div><p>Unable to retrieve expense!</p></div>);
+
+    console.log(expense);
 
     return (
-        <div>Test</div>
+        <div>
+            <ul>
+                <li>{expense[0].description}</li>
+                <li>{expense[0].source}</li>
+                <li>{expense[0].amount}</li>
+                <li>{expense[0].date}</li>
+            </ul>
+        </div>
     )
 }
 
