80927d1caae8eb84ad0adc0b634a7173687f54dd
PA4KEV <kevin.vd.vleuten@gmail.com>
2023-01-15 17:37:27 +0100

    Add sass module and colour overrides

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import './overrides.scss';
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import './App.css';
 import DataRetrieve from './UI/DataRetrieve';
 import DataSet from './UI/DataSet';
@@ -5,6 +8,7 @@ import DataSet from './UI/DataSet';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-primary">Expenses</h1>
       <DataSet/>
       <DataRetrieve/>
     </div>
