b5b41114c57479146c2d35299f0434b8a8aa8189
PA4KEV <kevin.vd.vleuten@gmail.com>
2023-01-15 17:23:49 +0100

    Allow insert of more data fields

--- frontend/src/UI/DataSet.js
+++ frontend/src/UI/DataSet.js
@@ -2,52 +2,62 @@ import React from 'react';
 import axios from 'axios';
 
 
-// const DataSet = (props) => {
-//     const [setExpense] = React.useState(null);
-
-//     function insertExpense() {
-//         const baseURL = "/insert_new_expense/";
-
-//         axios
-//           .post(baseURL, {
-//             description: "Razor Crest",
-//             source: "LEGO",
-//             amount: 139.99,
-//             date: "2023-01-15",
-//             time: "14:00"
-//           })
-//           .then((response) => {
-//             setExpense(response.data);
-//           });
-//     }
-
-//     return (
-//         <div>
-//             <button onClick={insertExpense}>Create Expense</button>
-//         </div>
-//     )
-// }
-
 class DataSet extends React.Component {
     state = {
-      name: ''
+      description: '',
+      source: '',
+      amount: '',
+      date: '',
+      time: ''
     }
   
-    handleChange = event => {
-      this.setState({ name: event.target.value });
+    handleChangeDescription = event => {
+        console.log("Handle change description");
+        this.setState({
+            description: event.target.value
+        });
+    }
+    handleChangeSource = event => {
+        console.log("Handle change source");
+        this.setState({
+            source: event.target.value
+        });
+    }
+    handleChangeAmount = event => {
+        console.log("Handle change amount");
+        this.setState({
+            amount: event.target.value
+        });
+    }
+    handleChangeDate = event => {
+        console.log("Handle change date");
+        this.setState({
+            date: event.target.value
+        });
+    }
+    handleChangeTime = event => {
+        console.log("Handle change time");
+        this.setState({
+            time: event.target.value
+        });
     }
   
     handleSubmit = event => {
+        event.preventDefault(); // Halt default form behaviour.
+
+        console.log("Handle submit");
+
         const baseURL = "http://localhost:8050/insert_new_expense";
-        event.preventDefault();
   
         const expense = {
-            description: this.state.name,
-            source: "TEST",
-            amount: 139.99,
-            date: "2023-01-15",
-            time: "14:55"
+            description: this.state.description,
+            source: this.state.source,
+            amount: parseFloat(this.state.amount),
+            date: this.state.date,
+            time: this.state.time
         };
+
+        console.log(expense);
   
         axios.post(baseURL, { expense })
         .then(res => {
@@ -61,16 +71,35 @@ class DataSet extends React.Component {
         <div>
           <form onSubmit={this.handleSubmit}>
             <label>
-              Expense:
-              <input type="text" name="name" onChange={this.handleChange} />
+              Description:
+              <input type="text" name="description" onChange={this.handleChangeDescription}/>
             </label>
+            <br/>
+            <label>
+              Source:
+              <input type="text" name="source" onChange={this.handleChangeSource}/>
+            </label>
+            <br/>
+            <label>
+              Amount:
+              <input type="text" name="amount" onChange={this.handleChangeAmount}/>
+            </label>
+            <br/>
+            <label>
+              Date:
+              <input type="text" name="date" onChange={this.handleChangeDate}/>
+            </label>
+            <br/>
+            <label>
+              Time:
+              <input type="text" name="time" onChange={this.handleChangeTime}/>
+            </label>
+            <br/>
             <button type="submit">Add Expense</button>
           </form>
         </div>
       )
     }
   }
-  
-
 
 export default DataSet;
